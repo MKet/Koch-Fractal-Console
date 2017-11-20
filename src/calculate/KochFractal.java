20442a688753577c58b4e9ab50db9d8069a72500
Marco Ketelaars <marco-ketelaars94@hotmail.com>
2017-11-20 11:17:04 +0100

    write edges to file

--- src/calculate/KochFractal.java
+++ src/calculate/KochFractal.java
@@ -16,7 +16,13 @@ public class KochFractal extends Observable {
     private int level = 1;      // The current level of the fractal
     private int nrOfEdges = 3;  // The number of edges in the current level of the fractal
     private float hue;          // Hue value of color for next edge
-    private boolean cancelled;  // Flag to indicate that calculation has been cancelled 
+    private boolean cancelled;  // Flag to indicate that calculation has been cancelled
+
+    public KochFractal(int level) {
+        setLevel(level);
+    }
+
+    public KochFractal() { }
 
     private void drawKochEdge(double ax, double ay, double bx, double by, int n) {
         if (!cancelled) {
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,59 @@
 package com.company;
 
-public class Main {
+import calculate.Edge;
+import calculate.KochFractal;
 
-    public static void main(String[] args) {
-	// write your code here
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Observable;
+import java.util.Observer;
+
+public class Main implements Observer {
+    FileOutputStream fos;
+    ObjectOutputStream oos;
+
+    public static void main(String[] args) throws IOException {
+	   Main main = new Main();
+	   main.Stuff();
+	   main.Close();
+
+	   System.out.println("Successful");
+    }
+
+    public void Stuff() throws IOException {
+        System.out.println("Koch level?!");
+        char read = (char) System.in.read();
+
+        int level = Integer.parseInt(""+ read);
+
+        System.out.println("Level " + level + " chosen.");
+
+        fos = new FileOutputStream("edges.kfr");
+        oos = new ObjectOutputStream(fos);
+
+        KochFractal fractal = new KochFractal(level);
+        fractal.addObserver(this);
+
+        System.out.println("generating bottom edge");
+        fractal.generateBottomEdge();
+        System.out.println("generating right edge");
+        fractal.generateRightEdge();
+        System.out.println("generating left edge");
+        fractal.generateLeftEdge();
+    }
+
+    public void Close() throws IOException {
+        oos.close();
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        try {
+            Edge edge = (Edge)arg;
+            oos.writeObject(edge);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
