4df129b31b8dcd1ce0f20a513a1a51d48503ceea
Marco Ketelaars <marco-ketelaars94@hotmail.com>
2017-12-04 20:04:45 +0100

    add locking and starting int

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,7 @@ import java.io.*;
 import java.nio.Buffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
 import java.nio.file.Files;
 import java.util.Observable;
 import java.util.Observer;
@@ -17,6 +18,8 @@ import java.util.Scanner;
 public class Main implements Observer {
     private MappedByteBuffer out;
     private static final int EDGE_SIZE = 7*8;
+    private FileChannel fc;
+    private FileLock lock;
 
     public static void main(String[] args) throws IOException {
 	   Main main = new Main();
@@ -45,10 +48,12 @@ public class Main implements Observer {
         KochFractal fractal = new KochFractal(level);
         fractal.addObserver(this);
 
-        File temp = new File("../temp");
-        RandomAccessFile randomAccessFile = new RandomAccessFile(temp, "rw");
-        FileChannel fc = randomAccessFile.getChannel();
-        out = fc.map(FileChannel.MapMode.READ_WRITE, 0, EDGE_SIZE * fractal.getNrOfEdges());
+        File file = new File("../"+level);
+        RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");
+        fc = randomAccessFile.getChannel();
+        out = fc.map(FileChannel.MapMode.READ_WRITE, 0, EDGE_SIZE * fractal.getNrOfEdges()+4);
+        lock = fc.lock(0, 4,false);
+        out.putInt(0);
 
         TimeStamp time = new TimeStamp();
         time.setBegin("calculation start");
@@ -60,25 +65,21 @@ public class Main implements Observer {
         fractal.generateLeftEdge();
         randomAccessFile.close();
 
-        sun.misc.Cleaner cleaner = ((DirectBuffer) out).cleaner();
-        cleaner.clean();
-
-        File file = new File("../"+level);
-
-        while (!temp.renameTo(file)) try {
-            Thread.sleep(10);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        time.setEnd();
-
+        time.setEnd("calculation end");
         System.out.println(time.toString());
     }
 
     @Override
     public void update(Observable o, Object arg) {
         Edge edge = (Edge) arg;
+        try {
+            long position = out.position();
+            FileLock l = lock;
+            lock = fc.lock(position, EDGE_SIZE, false);
+            l.release();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         out.putDouble(edge.X1);
         out.putDouble(edge.Y1);
         out.putDouble(edge.X2);
@@ -86,5 +87,18 @@ public class Main implements Observer {
         out.putDouble(edge.color.getRed());
         out.putDouble(edge.color.getBlue());
         out.putDouble(edge.color.getGreen());
+
+        try {
+            int position = out.position();
+            FileLock l = fc.lock(0, 4,false);
+            out.position(0);
+            int amount = out.getInt();
+            out.position(0);
+            out.putInt(amount+1);
+            out.position(position);
+            l.release();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
