33a2f91692e3eee2af3df1b2e79eecc1f5de128b
Marco Ketelaars <marco-ketelaars94@hotmail.com>
2017-12-12 10:15:13 +0100

    send edges via sockets

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,104 +1,91 @@
 package com.company;
 
-import calculate.Edge;
-import calculate.KochFractal;
-import sun.nio.ch.DirectBuffer;
+import calculate.*;
 import timeutil.TimeStamp;
 
 import java.io.*;
-import java.nio.Buffer;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.channels.FileLock;
-import java.nio.file.Files;
-import java.util.Observable;
-import java.util.Observer;
-import java.util.Scanner;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
-public class Main implements Observer {
-    private MappedByteBuffer out;
-    private static final int EDGE_SIZE = 7*8;
-    private FileChannel fc;
-    private FileLock lock;
+public class Main {
+    private List<Edge> edges;
+    private ObjectOutputStream outStream;
 
-    public static void main(String[] args) throws IOException {
-	   Main main = new Main();
-	   main.calculateFractal();
-	   System.out.println("Successful");
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+        while(true) {
+            Main main = new Main();
+            main.calculateFractal();
+            System.out.println("Successful");
+        }
     }
 
-    public void calculateFractal() throws IOException {
-        System.out.println("Koch level?!");
+    public void calculateFractal() throws IOException, ExecutionException, InterruptedException {
 
-        Scanner scanner = new Scanner(System.in);
-        int level = scanner.nextInt();
+        ServerSocket serverSocket = new ServerSocket(1337);
+
+        System.out.println("waiting for client connection");
+        ExecutorService pool = Executors.newCachedThreadPool();
+
+        while (true) {
+            Socket socket = serverSocket.accept();
+            pool.submit(() -> {
+                try {
+                    DoFractalTask(socket);
+                } catch (IOException | ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+    }
+
+    public void DoFractalTask(Socket incoming) throws IOException, ExecutionException, InterruptedException {
+        DataInputStream inStream = new DataInputStream(incoming.getInputStream());
+        outStream = new ObjectOutputStream(incoming.getOutputStream());
+
+        System.out.println("Scanning for level");
+        int level = inStream.readInt();
 
         System.out.println("Level " + level + " chosen.");
 
         calculateFractal(level);
-
     }
 
-    public void calculateFractals() throws IOException {
+    public void calculateFractals() throws IOException, ExecutionException, InterruptedException {
         for (int i = 1; i < 10; i++)
             calculateFractal(i);
     }
 
-    public void calculateFractal(int level) throws IOException {
-        KochFractal fractal = new KochFractal(level);
-        fractal.addObserver(this);
-
-        File file = new File("../"+level);
-        RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");
-        fc = randomAccessFile.getChannel();
-        out = fc.map(FileChannel.MapMode.READ_WRITE, 0, EDGE_SIZE * fractal.getNrOfEdges()+4);
-        lock = fc.lock(0, 4,false);
-        out.putInt(0);
-
+    public void calculateFractal(int level) throws IOException, ExecutionException, InterruptedException {
+        KochFractal fractal = new KochFractal();
         TimeStamp time = new TimeStamp();
         time.setBegin("calculation start");
-        System.out.println("generating bottom edge");
-        fractal.generateBottomEdge();
-        System.out.println("generating right edge");
-        fractal.generateRightEdge();
-        System.out.println("generating left edge");
-        fractal.generateLeftEdge();
-        randomAccessFile.close();
+
+        EdgeGenerator leftEdgeGenerator = new EdgeGenerator(level, EdgeType.Left);
+        EdgeGenerator rightEdgeGenerator = new EdgeGenerator(level, EdgeType.Right);
+        EdgeGenerator bottomEdgeGenerator = new EdgeGenerator(level, EdgeType.Bottom);
+
+        ExecutorService pool = Executors.newFixedThreadPool(3);
+        Future<List<Edge>> leftFuture = pool.submit(leftEdgeGenerator);
+        Future<List<Edge>> rightFuture = pool.submit(rightEdgeGenerator);
+        Future<List<Edge>> bottomFuture = pool.submit(bottomEdgeGenerator);
+
+        edges = new LinkedList<>();
+        edges.addAll(leftFuture.get());
+        edges.addAll(rightFuture.get());
+        edges.addAll(bottomFuture.get());
+
+        Answer answ = new Answer(level, edges);
+
+        outStream.writeObject(answ);
 
         time.setEnd("calculation end");
         System.out.println(time.toString());
-    }
-
-    @Override
-    public void update(Observable o, Object arg) {
-        Edge edge = (Edge) arg;
-        try {
-            long position = out.position();
-            FileLock l = lock;
-            lock = fc.lock(position, EDGE_SIZE, false);
-            l.release();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        out.putDouble(edge.X1);
-        out.putDouble(edge.Y1);
-        out.putDouble(edge.X2);
-        out.putDouble(edge.Y2);
-        out.putDouble(edge.color.getRed());
-        out.putDouble(edge.color.getBlue());
-        out.putDouble(edge.color.getGreen());
-
-        try {
-            int position = out.position();
-            FileLock l = fc.lock(0, 4,false);
-            out.position(0);
-            int amount = out.getInt();
-            out.position(0);
-            out.putInt(amount+1);
-            out.position(position);
-            l.release();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
